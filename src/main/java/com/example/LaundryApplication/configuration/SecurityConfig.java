65037b7e1395948ffaeabd5ace7f4c4b6d1d9c52
Dhruv khurana <Dhruvkhurana162@gmail.com>
2026-05-15 13:30:42 +0530

    CORS

--- src/main/java/com/example/LaundryApplication/configuration/SecurityConfig.java
+++ src/main/java/com/example/LaundryApplication/configuration/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("https://laundry-management-frontend.vercel.app/"));
+        config.setAllowedOrigins(List.of("https://laundry-management-frontend.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
--- src/main/java/com/example/LaundryApplication/controller/DashboardController.java
+++ src/main/java/com/example/LaundryApplication/controller/DashboardController.java
@@ -3,7 +3,6 @@ package com.example.LaundryApplication.controller;
 import com.example.LaundryApplication.dto.response.DashboardResponse;
 import com.example.LaundryApplication.service.OrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
