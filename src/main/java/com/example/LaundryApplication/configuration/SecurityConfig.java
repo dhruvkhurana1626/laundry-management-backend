aaa205808d2209e47905ac788605397af411796c
Dhruv khurana <Dhruvkhurana162@gmail.com>
2026-05-15 14:28:53 +0530

    Security Bean Changed

--- src/main/java/com/example/LaundryApplication/configuration/SecurityConfig.java
+++ src/main/java/com/example/LaundryApplication/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.LaundryApplication.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -24,13 +25,13 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf->csrf.disable())
-                .cors(cors -> {})
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/dashboard/**").hasRole("ADMIN")
                         .requestMatchers("/api/v1/orders/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
-                .httpBasic(httpBasic -> {});
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
