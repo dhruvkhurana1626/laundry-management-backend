489bf13fab1818baecee24a96fa4797c6eb6fa85
Dhruv khurana <Dhruvkhurana162@gmail.com>
2026-09-16 17:10:43 +0530

    Added Register/Login Feature

--- src/main/java/com/example/LaundryApplication/configuration/dto/request/LoginRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.LaundryApplication.configuration.dto.request;
-
-public class LoginRequest {
-
-    private String email;
-    private String password;
-
-}
--- src/main/java/com/example/LaundryApplication/configuration/dto/response/LoginResponse.java
+++
@@ -1,4 +0,0 @@
-package com.example.LaundryApplication.configuration.dto.response;
-
-public class LoginResponse {
-}
